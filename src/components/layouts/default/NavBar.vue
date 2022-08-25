ac84eca386d532b13360d34a9f4f89216ac60868
fexli <xiliang2002@sina.com>
2022-08-25 13:36:32 +0800

    fix z miss

--- src/components/layouts/default/NavBar.vue
+++ src/components/layouts/default/NavBar.vue
@@ -4,20 +4,17 @@ import DarkMode from "./nav/DarkMode.vue";
 import FullScreen from "./nav/FullScreen.vue";
 import Translate from "./nav/Translate.vue";
 import WebSocket from "./nav/WebSocket.vue";
-import Logo from "./nav/Logo.vue";
 import BreadCrumb from "./nav/BreadCrumb.vue";
 import Avatar from "./nav/Avatar.vue";
-import {globWidth} from "../../../plugins/common";
 
 </script>
 
 <template>
-  <div class="z-10 nav-h-lim h-12 lg:m-4 lg:rounded-xl bg-base-200 bg-opacity-80 backdrop-blur shadow-lg fixed navbar top-0 inset-x-0
+  <div class="z-40 nav-h-lim h-12 lg:m-4 lg:rounded-xl bg-base-200 bg-opacity-80 backdrop-blur shadow-lg fixed navbar top-0 inset-x-0
       w-full fixed transition-all lg:w-auto">
     <div class="nav-flex-cc">
       <Drawer/>
       <BreadCrumb/>
-      W:{{ globWidth }}
     </div>
     <div class="spacer"/>
     <div class="nav-flex-cc text-right">
